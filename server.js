3531688b1a1b57f44f57ed39563c0854f6bd0ce7
simoamogit <simoneinver@gmail.com>
2025-02-22 12:16:37 +0100

    Prima versione della webapp

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ Server avviato su http://localhost:${PORT}`);
+});
